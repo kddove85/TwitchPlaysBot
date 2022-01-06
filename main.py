d373ce8c73e5230b42e6b62032a791b157def9e0
unknown <kddove85@gmail.com>
2022-01-06 11:21:11 -0500

    Initial Commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import time
 import pywinauto
-from pywinauto.keyboard import send_keys
 from pynput.keyboard import Key, Controller
 from twitchio.ext import commands
 
@@ -16,6 +15,19 @@ class Bot(commands.Bot):
                          )
         self.keyboard = Controller()
 
+    def press_button(self, button, button_two=None):
+        app = pywinauto.Application().connect(title="Chrono Trigger").top_window()
+        app.set_focus()
+        if button_two:
+            with self.keyboard.pressed(button):
+                self.keyboard.press(button_two)
+                time.sleep(5)
+                self.keyboard.release(button)
+        else:
+            self.keyboard.press(button)
+            time.sleep(0.25)
+            self.keyboard.release(button)
+
     async def event_ready(self):
         # We are logged in and ready to chat and use commands...
         print(f'Logged in as | {self.nick}')
@@ -26,27 +38,79 @@ class Bot(commands.Bot):
         await ctx.send(f'Hello {ctx.author.name}!')
 
     @commands.command()
-    async def do_something(self, ctx: commands.Context):
-        # app = pywinauto.Application().connect(title="FINAL FANTASY V")
-        # app = pywinauto.Application().connect(title="Chrono Trigger").top_window()
-        # app.set_focus()
-        time.sleep(2)
-
-        # pynput
-        self.keyboard.press('v')
-        self.keyboard.release('v')
-
-        # pywinauto
-        # app.type_keys("v")
-        # send_keys("v")
+    async def list_commands(self, ctx: commands.Context):
+        message = f' +++++++ COMMANDS +++++++ ' \
+                  f'!save ---------------------------------------. ' \
+                  f'!confirm ------------------------------------. ' \
+                  f'!cancel -------------------------------------. ' \
+                  f'!menu --------------------------------------. ' \
+                  f'!auto_battle --------------------------------. ' \
+                  f'!flee ----------------------------------------. ' \
+                  f'!left ----------------------------------------. ' \
+                  f'!right ----------------------------------------. ' \
+                  f'!up -----------------------------------------. ' \
+                  f'!down ---------------------------------------. '
+        await ctx.send(message)
 
     @commands.command()
-    async def do_something_else(self, ctx: commands.Context):
-        app = pywinauto.Application().connect(title="Chrono Trigger").top_window()
-        app.set_focus()
-        self.keyboard.press('v')
+    async def save(self, ctx: commands.Context):
+        await self.menu(ctx)
         time.sleep(1)
-        self.keyboard.release('v')
+        await self.up(ctx)
+        time.sleep(1)
+        await self.confirm(ctx)
+        time.sleep(1)
+        await self.confirm(ctx)
+        time.sleep(1)
+        await self.up(ctx)
+        time.sleep(1)
+        await self.confirm(ctx)
+        time.sleep(2)
+        await self.cancel(ctx)
+        time.sleep(1)
+        await self.cancel(ctx)
+
+        self.press_button("x")
+
+    @commands.command()
+    async def confirm(self, ctx: commands.Context):
+        self.press_button("x")
+
+    @commands.command()
+    async def cancel(self, ctx: commands.Context):
+        self.press_button("c")
+
+    @commands.command()
+    async def menu(self, ctx: commands.Context):
+        self.press_button("v")
+
+    @commands.command()
+    async def auto_battle(self, ctx: commands.Context):
+        self.press_button("v")
+
+    @commands.command()
+    async def pause(self, ctx: commands.Context):
+        self.press_button(Key.space)
+
+    @commands.command()
+    async def flee(self, ctx: commands.Context):
+        self.press_button("g", button_two="h")
+
+    @commands.command()
+    async def left(self, ctx: commands.Context):
+        self.press_button("a")
+
+    @commands.command()
+    async def right(self, ctx: commands.Context):
+        self.press_button("d")
+
+    @commands.command()
+    async def up(self, ctx: commands.Context):
+        self.press_button("w")
+
+    @commands.command()
+    async def down(self, ctx: commands.Context):
+        self.press_button("s")
 
 
 bot = Bot()
